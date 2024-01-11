9cf4bc05564460c53f846c762e9792abc3c02a38
铍盐甜不甜 <51872789+long45343@users.noreply.github.com>
2024-01-11 09:35:45 +0800

    Add files via upload

--- mergeA&V.bat
+++ mergeA&V.bat
@@ -8,7 +8,15 @@ del %~n1.m4a
 ren %~n1-2.mp4 %~n1.mp4
 pause
 exit
+:flacprocess
+ffmpeg -i %~n1.mp4 -i %~n1.flac -codec copy %~n1-2.mp4
+del %~n1.mp4
+del %~n1.flac
+ren %~n1-2.mp4 %~n1.mp4
+pause
+exit
 :fileNotFound
+if exist "%fileBaseName%.flac" goto :flacprocess
 echo 输入文件格式错误/两个文件名字不一样/只有一个文件！
 pause
 exit
