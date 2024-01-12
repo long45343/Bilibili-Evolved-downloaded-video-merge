489d88c45d3062d484fe778970eea82c7b2bba65
铍盐甜不甜 <51872789+long45343@users.noreply.github.com>
2024-01-12 10:44:59 +0800

    Add files via upload

--- mergeA&V.bat
+++ mergeA&V.bat
@@ -2,17 +2,17 @@
 set "fileBaseName=%~n1"
 if not exist "%fileBaseName%.m4a" goto :fileNotFound
 if not exist "%fileBaseName%.mp4" goto :fileNotFound
-ffmpeg -i %~n1.mp4 -i %~n1.m4a -codec copy %~n1-2.mp4
-del %~n1.mp4
-del %~n1.m4a
-ren %~n1-2.mp4 %~n1.mp4
+ffmpeg -i "%fileBaseName%.mp4" -i "%fileBaseName%.m4a" -codec copy "%~n1-2.mp4"
+del "%fileBaseName%.mp4"
+del "%fileBaseName%.m4a"
+ren "%fileBaseName%-2.mp4" "%fileBaseName%.mp4"
 pause
 exit
 :flacprocess
-ffmpeg -i %~n1.mp4 -i %~n1.flac -codec copy %~n1-2.mp4
-del %~n1.mp4
-del %~n1.flac
-ren %~n1-2.mp4 %~n1.mp4
+ffmpeg -i "%fileBaseName%.mp4" -i "%fileBaseName%.flac" -codec copy "%fileBaseName%-2.mp4"
+del "%fileBaseName%.mp4"
+del "%fileBaseName%.flac"
+ren "%fileBaseName%-2.mp4" "%fileBaseName%.mp4"
 pause
 exit
 :fileNotFound
